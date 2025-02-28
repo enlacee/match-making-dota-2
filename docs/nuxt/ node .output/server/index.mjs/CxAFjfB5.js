6d65a23b130f683a546bcbe32f9372e5234e2535
Anibal Copitan <github@pprios.com>
2025-02-27 21:23:04 -0500

    minor: npm run generate for production

---
+++ docs/nuxt/ node .output/server/index.mjs/CxAFjfB5.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./DlAUqK2U.js";import{c as s,o,a,U as c}from"./BnGLlnUp.js";const r={},n={class:"flex items-center justify-center bg-gray-100"},l={class:"w-full max-w-4xl bg-white px-6 py-8"};function _(e,f){return o(),s("div",n,[a("div",l,[c(e.$slots,"default")])])}const m=t(r,[["render",_]]);export{m as default};
